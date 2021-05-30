204768362238c14e719f8643e05c7a07d2d7d540
Ankur Pandey <matrixbegins@gmail.com>
2021-05-30 13:32:46 +0530

    saving micorsecond time in DB for all tables

--- devices/src/main/kotlin/hat/streaming/devices/modules/service/CompromisedSignalService.kt
+++ devices/src/main/kotlin/hat/streaming/devices/modules/service/CompromisedSignalService.kt
@@ -23,10 +23,10 @@ class CompromisedSignalService(private val jdbcTemplate: JdbcTemplate? = null,
         val insertSQL = """ INSERT INTO devices.compromised_signals 
             | (device_id, signal_type, signal_value, message_digest, device_timestamp, reported_year, 
             | reported_month, reported_day, reported_hour, reported_minute, reported_sec, created_at)
-            | VALUES (?,    ?,          ?,                  ?,      to_timestamp(?::decimal/1000),            
-            | date_part('year', to_timestamp(? / 1000)) , date_part('month', to_timestamp(? / 1000)),              
-            | date_part('day', to_timestamp(? / 1000)), date_part('hour', to_timestamp(? / 1000)),
-            | date_part('minute', to_timestamp(? / 1000)), date_part('second', to_timestamp(? / 1000)), now() ) ON CONFLICT DO NOTHING;
+            | VALUES (?,    ?,          ?,                  ?,      to_timestamp(?::decimal/1000000),            
+            | date_part('year', to_timestamp(?::decimal/1000000)) , date_part('month', to_timestamp(?::decimal/1000000)),              
+            | date_part('day', to_timestamp(?::decimal/1000000)), date_part('hour', to_timestamp(?::decimal/1000000)),
+            | date_part('minute', to_timestamp(?::decimal/1000000)), date_part('second', to_timestamp(?::decimal/1000000)), now() ) ON CONFLICT DO NOTHING;
         """.trimMargin()
 
         runBatchInsert(signals, insertSQL)
